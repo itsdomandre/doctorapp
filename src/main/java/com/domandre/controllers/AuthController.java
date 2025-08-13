01c83a29abb52f0a9096de20bc9a09eb1cc1ee3e
Domingos André <domandreqs@gmail.com>
2025-08-13 23:39:50 +0100

    Adjusts Auth+Jwt config

--- src/main/java/com/domandre/controllers/AuthController.java
+++ src/main/java/com/domandre/controllers/AuthController.java
@@ -99,5 +99,7 @@ public class AuthController {
 
         return ResponseEntity.ok("Logout successfully");
     }
+
     //TODO: [2]: Implementar confirmation(com token), password reset
+    //TODO: [3]: Implementar a invalidação de token, sem necessidade de "store"
 }
